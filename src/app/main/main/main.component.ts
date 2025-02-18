8ca3110b1df9e9bc505a8e6d7efb88cb8b84ee14
hibikishirou <hibikishirou@gmail.com>
2025-02-18 08:55:29 +0700

    update

--- src/app/main/main/main.component.ts
+++ src/app/main/main/main.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { MenuComponent } from '../menu/menu.component';
 import { UserService } from '../../redux/user.service';
 import { CommonModule } from '@angular/common';
-import { forkJoin, takeUntil } from 'rxjs';
+import { of, switchMap, takeUntil } from 'rxjs';
 import { CharacterService } from '../../redux/character.service';
 import { DestroyComponent } from '../../core/destroy/destroy.component';
 
@@ -13,27 +13,32 @@ import { DestroyComponent } from '../../core/destroy/destroy.component';
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
 })
-export class MainComponent extends DestroyComponent {
+export class MainComponent extends DestroyComponent implements OnInit {
   constructor(
     private readonly userService: UserService,
     private readonly characterService: CharacterService,
     private router: Router
   ) {
     super();
+  }
+  ngOnInit(): void {
     this.redirect();
   }
-
   redirect() {
     this.userService
       .checkLogin()
-      .pipe(takeUntil(this.destroy$))
+      .pipe(
+        takeUntil(this.destroy$),
+        switchMap((result) => {
+          if (!result) {
+            this.router.navigate(['user']);
+            return of(false);
+          }
+          return this.characterService.getCharacter();
+        })
+      )
       .subscribe({
         next: (result) => {
-          if (result) {
-            this.router.navigate(['character']);
-          } else {
-            this.router.navigate(['user']);
-          }
         },
       });
   }

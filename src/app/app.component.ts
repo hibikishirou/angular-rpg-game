be1ec7bb402af51a99430b690d4f73180ba3960a
hibikishirou <hibikishirou@gmail.com>
2025-02-23 17:07:35 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { StoreService } from './redux/store.service';
-import { IndexDBService } from './core/index-db.service';
 
 @Component({
   selector: 'app-root',
@@ -13,7 +12,6 @@ export class AppComponent {
   title = 'angular-rpg-game';
   constructor(
     private readonly storeService: StoreService,
-    private indexDbService: IndexDBService
   ) {
     this.storeService.initStore();
     this.storeService.storeManagement();
